03a92b6cb46747a700d5d23f08a6205c239ba25e
OksanaNevolina <onevolina5gmail.com>
2024-09-21 20:03:38 +0300

    added validation for the login and password

--- src/modules/auth/dto/request/auth-base.request.dto.ts
+++ src/modules/auth/dto/request/auth-base.request.dto.ts
@@ -1,11 +1,16 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import {IsNotEmpty, IsString, Length, Matches} from 'class-validator';
+import {ApiProperty} from "@nestjs/swagger";
 
 export class AuthBaseRequestDto {
-  @IsNotEmpty()
-  @IsString()
-  readonly email: string;
+    @ApiProperty({ example: 'test@gmail.com' })
+    @IsString()
+    @Length(0, 300)
+    @Matches(/^[^\s@]+@([^\s@.,]+\.)+[^\s@.,]{2,}$/)
+    email: string;
 
-  @IsNotEmpty()
-  @IsString()
-  readonly password: string;
+    @ApiProperty({ example: '123qwe!@#QWE' })
+    @IsString()
+    @Length(0, 300)
+    @Matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%_*#?&])[A-Za-z\d@$_!%*#?&]{8,}$/)
+    password: string;
 }
